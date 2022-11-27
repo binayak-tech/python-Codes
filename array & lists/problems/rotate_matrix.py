0c5c7a084c18e050a4ce1003b05f8779921a2ce3
Binayak Purohit <93867057+binayak-tech@users.noreply.github.com>
2022-11-27 22:29:25 +0530

    Update rotate_matrix.py

--- array & lists/problems/rotate_matrix.py
+++ array & lists/problems/rotate_matrix.py
@@ -24,15 +24,11 @@ def matrix_rotation(mat):
             mat[bottom -i][left] = mat[bottom][right - i]
             mat[bottom][right - i] = mat[top + i][right]
             mat[top + i][right] = temp
-            left += 1
-            right -= 1
+        left += 1
+        right -= 1
     return mat
 
-mat = [
-[1,2,3],
-[4,5,6],
-[7,8,9]
-]
+mat = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
 new_mat = matrix_rotation(mat)
 
 for i in new_mat:
