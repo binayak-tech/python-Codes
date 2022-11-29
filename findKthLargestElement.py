e4e8d669ffa63c4fa28aa9ee5808f32695e3ac9d
Binayak Purohit <93867057+binayak-tech@users.noreply.github.com>
2022-11-29 23:14:33 +0530

    Add files via upload

---
+++ findKthLargestElement.py
@@ -0,0 +1,24 @@
+def findKthLargest(nums,k):
+    # APPROACH 1
+    nums.sort(reverse = True)
+    return nums[k - 1]
+
+
+    # APPROACH 2 USING QUICK SELECT ALGORITHM
+
+    # k = len(nums) - k
+
+    # def quickSelect(l, r):
+    #     pivot, p = nums[r], l
+    #     for i in range(l, r):
+    #         if nums[i] <= pivot:
+    #             nums[p], nums[i] = nums[i], nums[p]
+    #             p += 1
+    #     nums[p], nums[r] = nums[r], nums[p]
+
+    #     if p > k: return quickSelect(l, p-1)
+    #     elif p < k: return quickSelect(p + 1, r)
+    #     else: return nums[p]
+
+    # return quickSelect(0, len(nums) - 1)
+        
